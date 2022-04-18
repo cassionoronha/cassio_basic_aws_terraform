3733809e30c0bc50b5126f7c00bd5b4b50b8accd
Cassio Noronha <cassionoronha@gmail.com>
2022-04-18 19:29:53 -0300

    terraform basic configurations

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "productname" {
+  default = "cassio_basic_aws_terraform"
+}
+
+variable "aws_profile" {}
+
+variable "suffix" {}
+
+#VPC
+variable "vpc_cidr_block" {}
+variable "public_a_cidr_block" {}
